aa0a0c54a8e95a581efc82d0f5dcc0808ba3bae9
Warren R Bank <warren.r.bank@gmail.com>
2022-01-24 15:31:05 -0800

    v1.1.0: add user-input JSON validation

--- window-navigator/options_ui/options.js
+++ window-navigator/options_ui/options.js
@@ -35,19 +35,66 @@ var save_nav_value = function() {
   var $nav_value = document.getElementById('nav_value')
   var  nav_value = $nav_value.value
 
-  chrome.storage.sync.set(
-    {"nav_value": nav_value},
-    function() {
-      // Update status to let user know options were saved.
-      var $status = document.getElementById('status')
-      $status.textContent = 'Options saved.'
+  if (validate_nav_value(nav_value)) {
+    chrome.storage.sync.set(
+      {"nav_value": nav_value},
+      function() {
+        set_status_with_timer('Options saved.')
+      }
+    )
+  }
+  else {
+    set_status_with_timer('Invalid JSON.')
+  }
+}
 
-      // Clear status message after a brief period of time
-      setTimeout(function() {
-        $status.textContent = ''
-      }, 750)
-    }
+var validate_nav_value = function(nav_value) {
+  var nav
+  try {
+    nav = JSON.parse(nav_value)
+    if (!(nav instanceof Object)) throw ''
+    return true
+  }
+  catch(e) {
+    return false
+  }
+}
+
+var status_timer = 0
+var set_status_timer = function($status, delay) {
+  if (!($status instanceof HTMLElement)) {
+    $status = document.getElementById('status')
+  }
+
+  if (typeof delay !== 'number') {
+    delay = 1500
+  }
+
+  if (status_timer !== 0) {
+    clearTimeout(status_timer)
+  }
+
+  status_timer = setTimeout(
+    function() {
+      $status.textContent = ''
+      status_timer = 0
+    },
+    delay
   )
+}
+
+var set_status_with_timer = function(status_message, $status, delay) {
+  if (typeof status_message !== 'string') return
+
+  if (!($status instanceof HTMLElement)) {
+    $status = document.getElementById('status')
+  }
+
+  // Update status
+  $status.textContent = status_message
+
+  // Clear status after a delay
+  set_status_timer($status, delay)
 }
 
 var add_events = function() {
